fd8dbd2cd533104bfdee0320d319f3fd007ee46f
Austin <austinpedersen@ymail.com>
2020-12-14 16:49:02 -0500

    added markers to help point to boxes on the board

--- testChess.rb
+++ testChess.rb
@@ -3,8 +3,7 @@
 #Class Gamepiece, sub classes for pieces that can only move in definite ways vs picking a spot in a line
 class GamePiece
     attr_accessor :type, :color
-    def initialize(type, color)
-        @type = type
+    def initialize(color)
         @color = color
     end
 end
@@ -106,53 +105,54 @@ class Game
     def place_black_pieces
         x = 48
         while x < 56 do
-            @game_board.grid[x][2] = Pawn.new("pawn", "black")
+            @game_board.grid[x][2] = Pawn.new("black")
             x+=1
         end
-        @game_board.grid[56][2] = Rook.new("rook", "black")
-        @game_board.grid[63][2] = Rook.new("rook", "black")
-        @game_board.grid[57][2] = Knight.new("knight", "black")
-        @game_board.grid[62][2] = Knight.new("knight", "black")
-        @game_board.grid[58][2] = Bishop.new("bishop", "black")
-        @game_board.grid[61][2] = Bishop.new("bishop", "black")
-        @game_board.grid[59][2] = Queen.new("queen", "black")
-        @game_board.grid[60][2] = King.new("king", "black")
+        @game_board.grid[56][2] = Rook.new("black")
+        @game_board.grid[63][2] = Rook.new("black")
+        @game_board.grid[57][2] = Knight.new("black")
+        @game_board.grid[62][2] = Knight.new("black")
+        @game_board.grid[58][2] = Bishop.new("black")
+        @game_board.grid[61][2] = Bishop.new("black")
+        @game_board.grid[59][2] = Queen.new("black")
+        @game_board.grid[60][2] = King.new("black")
     end
 
     def place_white_pieces
         x = 8
         while x < 16 do
-            @game_board.grid[x][2] = Pawn.new("pawn", "white")
+            @game_board.grid[x][2] = Pawn.new("white")
             x+=1
         end
-        @game_board.grid[0][2] = Rook.new("rook", "white")
-        @game_board.grid[7][2] = Rook.new("rook", "white")
-        @game_board.grid[1][2] = Knight.new("knight", "white")
-        @game_board.grid[6][2] = Knight.new("knight", "white")
-        @game_board.grid[2][2] = Bishop.new("bishop", "white")
-        @game_board.grid[5][2] = Bishop.new("bishop", "white")
-        @game_board.grid[3][2] = Queen.new("queen", "white")
-        @game_board.grid[4][2] = King.new("king", "white")
+        @game_board.grid[0][2] = Rook.new("white")
+        @game_board.grid[7][2] = Rook.new("white")
+        @game_board.grid[1][2] = Knight.new("white")
+        @game_board.grid[6][2] = Knight.new("white")
+        @game_board.grid[2][2] = Bishop.new("white")
+        @game_board.grid[5][2] = Bishop.new("white")
+        @game_board.grid[3][2] = Queen.new("white")
+        @game_board.grid[4][2] = King.new("white")
     end
 
     def display_board
-        puts "---------------------------------"
-        puts "| #{@game_board.grid[56][2]} | #{@game_board.grid[57][2]} | #{@game_board.grid[58][2]} | #{@game_board.grid[59][2]} | #{@game_board.grid[60][2]} | #{@game_board.grid[61][2]} | #{@game_board.grid[62][2]} | #{@game_board.grid[63][2]} |"
-        puts "---------------------------------"
-        puts "| #{@game_board.grid[48][2]} | #{@game_board.grid[49][2]} | #{@game_board.grid[50][2]} | #{@game_board.grid[51][2]} | #{@game_board.grid[52][2]} | #{@game_board.grid[53][2]} | #{@game_board.grid[54][2]} | #{@game_board.grid[55][2]} |"
-        puts "---------------------------------"
-        puts "| #{@game_board.grid[40][2]} | #{@game_board.grid[41][2]} | #{@game_board.grid[42][2]} | #{@game_board.grid[43][2]} | #{@game_board.grid[44][2]} | #{@game_board.grid[45][2]} | #{@game_board.grid[46][2]} | #{@game_board.grid[47][2]} |"
-        puts "---------------------------------"
-        puts "| #{@game_board.grid[32][2]} | #{@game_board.grid[33][2]} | #{@game_board.grid[34][2]} | #{@game_board.grid[35][2]} | #{@game_board.grid[36][2]} | #{@game_board.grid[37][2]} | #{@game_board.grid[38][2]} | #{@game_board.grid[39][2]} |"
-        puts "---------------------------------"
-        puts "| #{@game_board.grid[24][2]} | #{@game_board.grid[25][2]} | #{@game_board.grid[26][2]} | #{@game_board.grid[27][2]} | #{@game_board.grid[28][2]} | #{@game_board.grid[29][2]} | #{@game_board.grid[30][2]} | #{@game_board.grid[31][2]} |"
-        puts "---------------------------------"
-        puts "| #{@game_board.grid[16][2]} | #{@game_board.grid[17][2]} | #{@game_board.grid[18][2]} | #{@game_board.grid[19][2]} | #{@game_board.grid[20][2]} | #{@game_board.grid[21][2]} | #{@game_board.grid[22][2]} | #{@game_board.grid[23][2]} |"
-        puts "---------------------------------"
-        puts "| #{@game_board.grid[8][2]} | #{@game_board.grid[9][2]} | #{@game_board.grid[10][2]} | #{@game_board.grid[11][2]} | #{@game_board.grid[12][2]} | #{@game_board.grid[13][2]} | #{@game_board.grid[14][2]} | #{@game_board.grid[15][2]} |"
-        puts "---------------------------------"
-        puts "| #{@game_board.grid[0][2]} | #{@game_board.grid[1][2]} | #{@game_board.grid[2][2]} | #{@game_board.grid[3][2]} | #{@game_board.grid[4][2]} | #{@game_board.grid[5][2]} | #{@game_board.grid[6][2]} | #{@game_board.grid[7][2]} |"
-        puts "---------------------------------"
+        puts "  ---------------------------------"
+        puts "H | #{@game_board.grid[56][2]} | #{@game_board.grid[57][2]} | #{@game_board.grid[58][2]} | #{@game_board.grid[59][2]} | #{@game_board.grid[60][2]} | #{@game_board.grid[61][2]} | #{@game_board.grid[62][2]} | #{@game_board.grid[63][2]} |"
+        puts "  ---------------------------------"
+        puts "G | #{@game_board.grid[48][2]} | #{@game_board.grid[49][2]} | #{@game_board.grid[50][2]} | #{@game_board.grid[51][2]} | #{@game_board.grid[52][2]} | #{@game_board.grid[53][2]} | #{@game_board.grid[54][2]} | #{@game_board.grid[55][2]} |"
+        puts "  ---------------------------------"
+        puts "F | #{@game_board.grid[40][2]} | #{@game_board.grid[41][2]} | #{@game_board.grid[42][2]} | #{@game_board.grid[43][2]} | #{@game_board.grid[44][2]} | #{@game_board.grid[45][2]} | #{@game_board.grid[46][2]} | #{@game_board.grid[47][2]} |"
+        puts "  ---------------------------------"
+        puts "E | #{@game_board.grid[32][2]} | #{@game_board.grid[33][2]} | #{@game_board.grid[34][2]} | #{@game_board.grid[35][2]} | #{@game_board.grid[36][2]} | #{@game_board.grid[37][2]} | #{@game_board.grid[38][2]} | #{@game_board.grid[39][2]} |"
+        puts "  ---------------------------------"
+        puts "D | #{@game_board.grid[24][2]} | #{@game_board.grid[25][2]} | #{@game_board.grid[26][2]} | #{@game_board.grid[27][2]} | #{@game_board.grid[28][2]} | #{@game_board.grid[29][2]} | #{@game_board.grid[30][2]} | #{@game_board.grid[31][2]} |"
+        puts "  ---------------------------------"
+        puts "C | #{@game_board.grid[16][2]} | #{@game_board.grid[17][2]} | #{@game_board.grid[18][2]} | #{@game_board.grid[19][2]} | #{@game_board.grid[20][2]} | #{@game_board.grid[21][2]} | #{@game_board.grid[22][2]} | #{@game_board.grid[23][2]} |"
+        puts "  ---------------------------------"
+        puts "B | #{@game_board.grid[8][2]} | #{@game_board.grid[9][2]} | #{@game_board.grid[10][2]} | #{@game_board.grid[11][2]} | #{@game_board.grid[12][2]} | #{@game_board.grid[13][2]} | #{@game_board.grid[14][2]} | #{@game_board.grid[15][2]} |"
+        puts "  ---------------------------------"
+        puts "A | #{@game_board.grid[0][2]} | #{@game_board.grid[1][2]} | #{@game_board.grid[2][2]} | #{@game_board.grid[3][2]} | #{@game_board.grid[4][2]} | #{@game_board.grid[5][2]} | #{@game_board.grid[6][2]} | #{@game_board.grid[7][2]} |"
+        puts "  ---------------------------------"
+        puts "    1   2   3   4   5   6   7   8"
     end
 end
 game = Game.new
